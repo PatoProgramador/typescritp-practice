080b98b2d048bf548c711e2397256288ae11d36a
PatoProgramador <spartan1171947@hotmail.com>
2023-05-29 22:10:26 -0500

    basic operation

---
+++ calculator.ts
@@ -0,0 +1,7 @@
+const operations = ['multiply', 'add', 'divide'];
+
+const calculator = (a:number, b: number, op: string) => {
+    if(operations.includes(op)){
+        console.log()
+    }
+}
---
+++ multiplier.ts
@@ -0,0 +1,6 @@
+const multiplicator = (a: number, b: number) => {
+    let prinText: string = `Multiplied ${a} and ${b}, the result is: `;
+    console.log(prinText, a * b)
+};
+
+multiplicator(2, 4);
